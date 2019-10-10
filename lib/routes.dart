43a2d0e4f85d5e6fde01c2540895f14047072403
Abdulmohsen Alkhamees <abdulmohsen.alkhamees@kw.zain.com>
2019-10-10 20:43:42 +0300

    finalizing

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_for_web_part1/main.dart';
 import 'dart:html' as html;
+import 'dart:js' as js;
 
 const String homePageRoute = '/';
 const String internalRoute = '/internal';
@@ -32,12 +33,11 @@ class Router {
                 settings.arguments as ExternalRouteArguments;
 
             if (arguments != null) {
+              //This is where dart to html proxying happens
               if (arguments.showAlertMessage)
-                //this is where dart to html proxying happens
                 html.window
                     .alert("You're being redirected to \n ${arguments.url}");
-              html.window.location.href = arguments.url;
-              // return Container();
+              js.context.callMethod("open", [arguments.url]);
             }
             //For simplicity we will go bk to home page again, you can decide how you handle it
             return MyHomePage(
