1153814fd9a6bc3178fc8af577094314ec4aedf0
Abdulmohsen Alkhamees <abdulmohsen.alkhamees@kw.zain.com>
2019-10-12 01:31:18 +0300

    adding comments

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
                 //Executes javascripts console.log();
                 js.context["console"].callMethod(
-                    "log", ['internalRoute', '\n', jsonObject["message"]]);
+                    "log", ['still in the same page', '\n', jsonObject["message"]]);
               },
               child: Image.network(
                 "https://flutter.dev/assets/flutter-lockup-4cb0ee072ab312e59784d9fbf4fb7ad42688a7fdaea1270ccf6bbf4f34b7e03f.svg",
